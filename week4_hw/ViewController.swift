417dd17e00f66b9ddbb7dd7d5cd9fa6c39d8f6aa
Sunny Li <sunnbunz@fb.com>
2016-06-08 16:54:39 -0700

    Initial Commit

---
+++ week4_hw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week4_hw
+//
+//  Created by Sunny Li on 6/8/16.
+//  Copyright © 2016 Sunny Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
